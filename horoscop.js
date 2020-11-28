4f7dced7b1970d3440249d032ca2a03f896dcb67
Hamide Bay <hamide.bay@baycom.ch>
2020-11-29 00:32:30 +0100

    horoscop code eingefügt

---
+++ horoscop.js
@@ -0,0 +1,15 @@
+const month = 3;
+const day = 12;
+
+if (((month == 3) && (day >= 21)) || ((month == 4) && (day <= 20))) { console.log("Koc"); }
+else if (((month == 4) && (day >= 21)) || ((month == 5) && (day <= 20))) { console.log("Boga"); }
+else if (((month == 5) && (day >= 21)) || ((month == 6) && (day <= 20))) { console.log("Ikizler"); }
+else if (((month == 6) && (day >= 21)) || ((month == 7) && (day <= 20))) { console.log("Yengec"); }
+else if (((month == 7) && (day >= 21)) || ((month == 8) && (day <= 20))) { console.log("Aslan"); }
+else if (((month == 8) && (day >= 21)) || ((month == 9) && (day <= 20))) { console.log("Basak"); }
+else if (((month == 9) && (day >= 21)) || ((month == 10) && (day <= 20))) { console.log("Terazi"); }
+else if (((month == 10) && (day >= 21)) || ((month == 11) && (day <= 20))) { console.log("Akrep"); }
+else if (((month == 11) && (day >= 21)) || ((month == 12) && (day <= 20))) { console.log("Yay"); }
+else if (((month == 12) && (day >= 21)) || ((month == 1) && (day <= 20))) { console.log("Oglak"); }
+else if (((month == 1) && (day >= 21)) || ((month == 2) && (day <= 20))) { console.log("Kova"); }
+else { console.log("Balik"); }
