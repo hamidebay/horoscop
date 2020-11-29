be00e6f9cb0a8c282782dc9a63c44c6982f8d64f
Hamide Bay <hamide.bay@baycom.ch>
2020-11-29 15:08:12 +0100

    Error Correction on cabbasStyle

--- cabbarsStyle.js
+++ cabbarsStyle.js
@@ -1,11 +1,10 @@
-const marke = bmw;
-const color = red;
-const year = 2005;
-const customer_old = 3;
+const marke = "vw";
+const color = "black";
+const year = 2017;
+const customer_old = 1;
 
-
-if(customer_old >5 ){console.log("Araban tamir edilecek");}
-else if(color == (red) && (marke == bmw) && ( 2010 >= year <= 2019 )) {console.log("Araban tamir edilecek");}
-else if(color == (red || black) && (marke == audi) && ( (2005 >= year <= 2020 )) || (2014 >= year <= 2020 )) {console.log("Araban tamir edilecek");}
-else if(color == (black) && (marke == vw) && ( 2001 >= year <= 2018 )) {console.log("Araban tamir edilecek");}
-else {console.log("üzgünüm sizi kabul edemem");}
+if (customer_old > 5) { console.log("Araban tamir edilecek"); }
+else if ((marke == "bmw") && (color == "red") && ((year >= 2010) && (year <= 2019))) { console.log(marke + " tamir edilecek"); }
+else if ((marke == "audi") && ((color == "red") || (color == "black")) && (((year >= 2005) && (year <= 2010)) || ((year >= 2014) && (year <= 2020)))) { console.log(marke + " tamir edilecek"); }
+else if ((marke == "vw") && (color == "black") && ((year >= 2001) && (year <= 2018))) { console.log(marke + " tamir edilecek"); }
+else { console.log("üzgünüm sizi kabul edemem"); }
